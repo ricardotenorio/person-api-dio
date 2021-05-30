bdc6abbb1362de917cbadf4263e6dc01e22461fa
ricardotenorio <ricardo.heber@gmail.com>
2021-05-30 03:35:32 -0300

    first commit

---
+++ src/main/java/ricardotenorio/github/com/personapi/PersonapiApplication.java
@@ -0,0 +1,13 @@
+package ricardotenorio.github.com.personapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ricardotenorio/github/com/personapi/PersonapiApplicationTests.java
@@ -0,0 +1,13 @@
+package ricardotenorio.github.com.personapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
